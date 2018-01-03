250349d0bc17b600550dc50964a51888a4e970e7
Ryan Phillip Thomas <ryanphillipthomas@mac.com>
2018-01-03 00:08:27 -0500

    WIP: Lightning Tweaks

--- Ambit/Thunderstorm_Fireplace.swift
+++ Ambit/Thunderstorm_Fireplace.swift
@@ -11,6 +11,13 @@ import AudioPlayer
 import AVFoundation
 import MediaPlayer
 
+private enum FlashType {
+    case Default
+    case Sharp
+    case Trailing
+    case All
+}
+
 class Thunderstorm_Fireplace: NSObject {
     static let sharedManager = Thunderstorm_Fireplace()
     var currentSound: AudioPlayer?
@@ -18,6 +25,8 @@ class Thunderstorm_Fireplace: NSObject {
     var audioPlayerVolume : Float!
     var lightBrightnessLevel : Float!
     var isPlayingOverride : Bool = false
+    var isTransitioning : Bool = false
+
     
     func isPlayingSound() -> Bool {
         if isPlayingOverride { return true }
@@ -108,7 +117,19 @@ class Thunderstorm_Fireplace: NSObject {
     }
     
     fileprivate func largeFlashAll() {
-        lightsWhiteBright()
+        let index = Int(arc4random_uniform(UInt32(5)))
+        if index == 0 {
+            lightsWhiteBright(FlashType.Sharp)
+        } else if index == 1 {
+            lightsWhiteBright(FlashType.All)
+        } else if index == 2 {
+            lightsWhiteBright(FlashType.Default)
+        } else if index == 3 {
+            lightsWhiteBright(FlashType.Sharp)
+        } else if index == 4 {
+            lightsWhiteBright(FlashType.Trailing)
+        }
+
     }
     
     fileprivate func lightsBright() {
@@ -136,50 +157,200 @@ class Thunderstorm_Fireplace: NSObject {
     
     
     //grab a random light, grab a random brightness (dev todo add brighness levels), grab the color value before the update and set it back to it after...
-    fileprivate func lightsWhiteBright() {
+    fileprivate func lightsWhiteBright(_ type: FlashType) {
+        if self.isTransitioning {
+            return
+        }
+        
+        self.isTransitioning = true
         let cache = PHBridgeResourcesReader.readBridgeResourcesCache()
         let bridgeSendAPI = PHBridgeSendAPI()
-        
         if let lights = cache?.lights {
-            let lightsData = NSMutableDictionary()
-            let randomLightIndex = Int(arc4random_uniform(UInt32(lights.count)))
-            lightsData.addEntries(from: lights)
-            
-            let randomLight = lightsData.allValues[randomLightIndex]
-            let newLight = randomLight as! PHLight
-            let newLightX = newLight.lightState.value(forKey: "x")
-            let newLightY = newLight.lightState.value(forKey: "y")
-            let newLightBrightness = newLight.lightState.value(forKey: "brightness")
-            var newLightXY = CGPoint(x:0, y:0)
-
-            if (newLightY != nil) && (newLightX != nil) {
-                 newLightXY = CGPoint(x:newLightX as! CGFloat, y:newLightY as! CGFloat)
-            }
-            
-            let lightState = PHLightState()
-            
-            let xy = Utilities.calculateXY(cgColor: UIColor.white.cgColor, forModel: newLight.modelNumber)
-            
-            lightState.x = xy.x as NSNumber!
-            lightState.y = xy.y as NSNumber!
-            
-            lightState.brightness = 254 //0-254
-            lightState.setOn(true)
-            
-            bridgeSendAPI.updateLightState(forId: newLight.identifier, with: lightState, completionHandler: { (errors : [Any]?) in
+            switch type {
+            case .Default:
+                let lightsData = NSMutableDictionary()
+                let randomLightIndex = Int(arc4random_uniform(UInt32(lights.count)))
+                lightsData.addEntries(from: lights)
+                
+                let randomLight = lightsData.allValues[randomLightIndex]
+                let newLight = randomLight as! PHLight
+                let newLightX = newLight.lightState.value(forKey: "x")
+                let newLightY = newLight.lightState.value(forKey: "y")
+                let newLightBrightness = newLight.lightState.value(forKey: "brightness")
+                var newLightXY = CGPoint(x:0, y:0)
+                
+                if (newLightY != nil) && (newLightX != nil) {
+                    newLightXY = CGPoint(x:newLightX as! CGFloat, y:newLightY as! CGFloat)
+                }
+                
                 let lightState = PHLightState()
                 
-                let xy = newLightXY
+                let xy = Utilities.calculateXY(cgColor: UIColor.white.cgColor, forModel: newLight.modelNumber)
                 
                 lightState.x = xy.x as NSNumber!
                 lightState.y = xy.y as NSNumber!
                 
-                lightState.brightness = newLightBrightness as! NSNumber //0-254
+                lightState.brightness = 254 //0-254
                 lightState.setOn(true)
+                
                 bridgeSendAPI.updateLightState(forId: newLight.identifier, with: lightState, completionHandler: { (errors : [Any]?) in
-                    //
+                    let lightState = PHLightState()
+                    
+                    let xy = newLightXY
+                    lightState.brightness = newLightBrightness as! NSNumber //0-254
+                    lightState.x = xy.x as NSNumber!
+                    lightState.y = xy.y as NSNumber!
+                    
+                    lightState.setOn(true)
+                    bridgeSendAPI.updateLightState(forId: newLight.identifier, with: lightState, completionHandler: { (errors : [Any]?) in
+                        //
+                        print("stoped transitiong default")
+                        self.isTransitioning = false;
+                    })
                 })
-            })
+            case .Sharp:
+                print("sharp")
+                let lightsData = NSMutableDictionary()
+                let randomLightIndex = Int(arc4random_uniform(UInt32(lights.count)))
+                lightsData.addEntries(from: lights)
+                
+                let randomLight = lightsData.allValues[randomLightIndex]
+                let newLight = randomLight as! PHLight
+                let newLightX = newLight.lightState.value(forKey: "x")
+                let newLightY = newLight.lightState.value(forKey: "y")
+                let newLightBrightness = newLight.lightState.value(forKey: "brightness")
+                var newLightXY = CGPoint(x:0, y:0)
+                
+                if (newLightY != nil) && (newLightX != nil) {
+                    newLightXY = CGPoint(x:newLightX as! CGFloat, y:newLightY as! CGFloat)
+                }
+                
+                let lightState = PHLightState()
+                
+                let xy = Utilities.calculateXY(cgColor: UIColor.white.cgColor, forModel: newLight.modelNumber)
+                
+                lightState.x = xy.x as NSNumber!
+                lightState.y = xy.y as NSNumber!
+                
+                lightState.brightness = 254 //0-254
+                lightState.setOn(true)
+                
+                bridgeSendAPI.updateLightState(forId: newLight.identifier, with: lightState, completionHandler: { (errors : [Any]?) in
+                })
+                let newLightState = PHLightState()
+                newLightState.brightness = newLightBrightness as! NSNumber //0-254
+
+                let new_xy = newLightXY
+                newLightState.x = new_xy.x as NSNumber!
+                newLightState.y = new_xy.y as NSNumber!
+                
+                newLightState.setOn(true)
+                bridgeSendAPI.updateLightState(forId: newLight.identifier, with: newLightState, completionHandler: { (errors : [Any]?) in
+                    //
+                    print("stoped transitiong sharp")
+                    self.isTransitioning = false;
+                })
+            case .Trailing:
+                let lightsData = NSMutableDictionary()
+                let lightsValueArray = NSMutableArray()
+
+                lightsData.addEntries(from: lights)
+ 
+                //make light count dynamic
+                for _ in 0 ..< 3 {
+                    let randomIndex = Int(arc4random_uniform(UInt32(lightsData.allValues.count)))
+                    let randomItem = lightsData.allValues[randomIndex]
+                    lightsValueArray.add(randomItem)
+                }
+                
+                var count = 0
+                for light in lightsValueArray {
+                    let newLight = light as! PHLight
+                    let newLightX = newLight.lightState.value(forKey: "x")
+                    let newLightY = newLight.lightState.value(forKey: "y")
+                    let newLightBrightness = newLight.lightState.value(forKey: "brightness")
+                    var newLightXY = CGPoint(x:0, y:0)
+                    
+                    if (newLightY != nil) && (newLightX != nil) {
+                        newLightXY = CGPoint(x:newLightX as! CGFloat, y:newLightY as! CGFloat)
+                    }
+                    
+                    let lightState = PHLightState()
+                    
+                    let xy = Utilities.calculateXY(cgColor: UIColor.white.cgColor, forModel: newLight.modelNumber)
+                    
+                    lightState.x = xy.x as NSNumber!
+                    lightState.y = xy.y as NSNumber!
+                    
+                    lightState.brightness = 254 //0-254
+                    lightState.setOn(true)
+                    
+                    bridgeSendAPI.updateLightState(forId: newLight.identifier, with: lightState, completionHandler: { (errors : [Any]?) in
+                        let lightState = PHLightState()
+                        lightState.brightness = newLightBrightness as! NSNumber //0-254
+                        
+                        let xy = newLightXY
+                        lightState.x = xy.x as NSNumber!
+                        lightState.y = xy.y as NSNumber!
+                        
+                        lightState.setOn(true)
+                        bridgeSendAPI.updateLightState(forId: newLight.identifier, with: lightState, completionHandler: { (errors : [Any]?) in
+                            count += 1
+                            print(count)
+                            if count == lightsValueArray.count {
+                                print("stoped transitiong trailing")
+                                self.isTransitioning = false;
+                            }
+                        })
+                    })
+                    
+                }
+            case .All:
+                let lightsData = NSMutableDictionary()
+                lightsData.addEntries(from: lights)
+                var count = 0
+                for light in lightsData.allValues {
+                    let newLight = light as! PHLight
+                    let newLightX = newLight.lightState.value(forKey: "x")
+                    let newLightY = newLight.lightState.value(forKey: "y")
+                    let newLightBrightness = newLight.lightState.value(forKey: "brightness")
+                    var newLightXY = CGPoint(x:0, y:0)
+                    
+                    if (newLightY != nil) && (newLightX != nil) {
+                        newLightXY = CGPoint(x:newLightX as! CGFloat, y:newLightY as! CGFloat)
+                    }
+                    
+                    let lightState = PHLightState()
+                    
+                    let xy = Utilities.calculateXY(cgColor: UIColor.white.cgColor, forModel: newLight.modelNumber)
+                    
+                    lightState.x = xy.x as NSNumber!
+                    lightState.y = xy.y as NSNumber!
+                    
+                    lightState.brightness = 254 //0-254
+                    lightState.setOn(true)
+                    
+                    bridgeSendAPI.updateLightState(forId: newLight.identifier, with: lightState, completionHandler: { (errors : [Any]?) in
+                        let lightState = PHLightState()
+                        lightState.brightness = newLightBrightness as! NSNumber //0-254
+
+                        let xy = newLightXY
+                        lightState.x = xy.x as NSNumber!
+                        lightState.y = xy.y as NSNumber!
+                        
+                        lightState.setOn(true)
+                        bridgeSendAPI.updateLightState(forId: newLight.identifier, with: lightState, completionHandler: { (errors : [Any]?) in
+                            count += 1
+                            print(count)
+                            if count == lightsData.allValues.count {
+                                print("stoped transitiong all")
+                                self.isTransitioning = false;
+                            }
+                        })
+                    })
+                    
+                }
+            }
         }
     }
 }
